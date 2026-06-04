52ea151ffa7a3357db032d35439db0670302903d
sadique9334 <163935036+sadique9334@users.noreply.github.com>
2026-06-04 16:53:26 +0530

    Add reverse integer solution in reverse_integer.cpp
    Implement reverse integer function for LeetCode problem.

--- maths/reverse_integer.cpp
+++ maths/reverse_integer.cpp
@@ -1,1 +1,21 @@
-
+/*leetcode problem - 07
+  topic : reverse an integer number 
+  example:
+  Input: x = 123
+  Output: 321
+  */
+class Solution {
+public:
+    int reverse(int x) {
+        int answer=0;
+        while(x!=0){
+            if(answer>INT_MAX/10 || answer<INT_MIN/10){
+                return 0;
+                }
+            int digit=x%10;
+            answer=(answer*10)+digit;
+            x=x/10;
+        }
+        return answer;
+    }
+};
