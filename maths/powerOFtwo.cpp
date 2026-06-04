e3bd794c311dd9dec5abd9eb41312d8a6bea9c37
sadique9334 <163935036+sadique9334@users.noreply.github.com>
2026-06-04 21:11:53 +0530

    function to determine if n is a power of two
    Implement a function to check if a number is a power of two.

--- maths/powerOFtwo.cpp
+++ maths/powerOFtwo.cpp
@@ -1,1 +1,14 @@
-
+/*Given an integer n, return true if it is a power of two. Otherwise, return false.*/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for(int i=0;i<=30;i++){
+            int ans=pow(2,i);
+            if(ans==n){
+                return true;
+            }
+            }
+            return false;
+        
+    }
+};
