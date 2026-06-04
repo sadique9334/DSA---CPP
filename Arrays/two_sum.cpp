c53df2ad0fbd73f1eab7200e75ede5813ca6593a
sadique9334 <163935036+sadique9334@users.noreply.github.com>
2026-06-04 16:34:10 +0530

    Add Two Sum solution using nested loops
    Implemented a solution for the Two Sum problem using nested loops to find indices of two numbers that add up to the target.

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,1 +1,31 @@
+/*
+LeetCode 1 - Two Sum
+Difficulty: Easy
+Approach:
+- Use nested for loops
+- Store element and index
+- Check if sum== target exists
 
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       // vector<int> ans;
+        int size=nums.size();
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                
+                if(nums[i]+nums[j]==target){
+                    return{i,j};
+                    // ans.push_back(i);
+                    // ans.push_back(j);
+                }
+            }
+         
+        }
+        return {};
+    }
+};
