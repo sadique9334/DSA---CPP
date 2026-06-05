b6639348288b2aafca3acce19c58b09e7b55b2f2
sadique9334 <163935036+sadique9334@users.noreply.github.com>
2026-06-05 07:39:22 +0530

    Hamming weight function to count set bits
    Implemented a function to count the number of set bits in a positive integer's binary representation.

--- maths/numberOF1bits.cpp
+++ maths/numberOF1bits.cpp
@@ -1,1 +1,20 @@
-
+/*Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+Example 1:
+Input: n = 11
+Output: 3
+Explanation:
+The input binary string 1011 has a total of three set bits.*/
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+            if(n&1){
+            count++;
+            }
+            n=n>>1;
+        }
+        return count;
+        
+    }
+};
