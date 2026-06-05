bd4ae3d765d1cc624cc3c9b509a8e361edeec508
Sadique Anwar <163935036+sadique9334@users.noreply.github.com>
2026-06-05 10:20:12 +0530

    binary search for first and last positions
    Implement binary search to find first and last positions of a target in a sorted array.

--- searching and sorting/binary_search.cpp
+++ searching and sorting/binary_search.cpp
@@ -1,1 +1,53 @@
+/*Find First and Last Position of Element in Sorted Array
+Example :
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+*/
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
 
+        //first occurance
+        int start=0;
+        int end=nums.size()-1;
+        vector<int>ans;
+        int annn=-1;
+        int mid=start+ (end-start)/2;
+        while(start<=end){
+            if(nums[mid]==target){
+                annn=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            mid=start+(end-start)/2;
+        }
+
+        //second occurance
+ans.push_back(annn);
+start=0;
+end=nums.size()-1;
+mid=start+ (end-start)/2;
+annn=-1;
+while(start<=end){
+            if(nums[mid]==target){
+                annn=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            mid=start+(end-start)/2;
+        }
+        ans.push_back(annn);
+          return ans;
+    }
+  
+};
