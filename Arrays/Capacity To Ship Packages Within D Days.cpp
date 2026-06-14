df9fa8635540c391a74355c8bcd3f32ed6502863
Sadique Anwar <163935036+sadique9334@users.noreply.github.com>
2026-06-14 12:51:39 +0530

    Add solution for 'Capacity To Ship Packages Within D Days'
    Implement solution for shipping packages within given days.

---
+++ Arrays/Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,57 @@
+/*[1011] Capacity To Ship Packages Within D Days
+A conveyor belt has packages that must be shipped from one port to another within days days.
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+
+Example 1:
+Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+Output: 15
+Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+1st day: 1, 2, 3, 4, 5
+2nd day: 6, 7
+3rd day: 8
+4th day: 9
+5th day: 10*/
+
+bool ispossible(vector<int> weights,int days,int size,int mid){
+    int count=1;
+    int sumOfWeights=0;
+    for(int i=0;i<size;i++){
+        if(sumOfWeights+weights[i]<=mid){
+            sumOfWeights+=weights[i];
+        }
+        else{
+            count++;
+            if(count>days || weights[i]>mid){
+                return false;
+            }
+            sumOfWeights=weights[i];
+        }
+    }
+    return true;
+}
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int total_weights=0;
+        int s=0;
+        for(int i=0;i<weights.size();i++){
+            total_weights+=weights[i];
+        }
+        int e=total_weights;
+        int mid=s+(e-s)/2;
+        int ans=-1;
+        while(s<=e){
+        if(ispossible(weights,days,weights.size(),mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+        }
+        return ans;
+        
+    }
+};
