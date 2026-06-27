56149551dfc49ae55fccdee80d9db93589899434
Sadique Anwar <163935036+sadique9334@users.noreply.github.com>
2026-06-27 09:53:26 +0530

    Add stack example in C++ with basic operations

--- STL tools in CPP/stack.cpp
+++ STL tools in CPP/stack.cpp
@@ -1,3 +1,17 @@
 #include<iostream>
 #include<stack>
 using namespace std;
+int main(){
+    stack<int>s;
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+    cout<<"size = "<<s.size()<<endl;
+    cout<<"top element = "<<s.top()<<endl;
+    s.pop();
+    cout<<"top element = "<<s.top()<<endl;
+    cout<<"size = "<<s.size()<<endl;
+
+    cout<<"empty or not = "<<s.empty();
+}
