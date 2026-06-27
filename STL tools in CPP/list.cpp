99c0f4ca912cd8ad8d2c2d9c216b147ed0007442
Sadique Anwar <163935036+sadique9334@users.noreply.github.com>
2026-06-27 09:39:14 +0530

    Implement list operations in C++
    Added examples of list operations including push_back, push_front, and erase.

--- STL tools in CPP/list.cpp
+++ STL tools in CPP/list.cpp
@@ -1,1 +1,23 @@
-#
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+    list<int>l;
+    l.push_back(5);
+    l.push_back(4);
+    l.push_front(1);
+    for(int i:l)
+    cout<<i<<" "<<endl;
+    list<int>m(5,99);
+    for(int i:m)
+    cout<<i<<" ";
+    cout<<endl;
+    cout<<"size of l = "<<sizeof(l)<<endl;
+    cout<<"size of l = "<<sizeof(m)<<endl;
+    cout<<"capacity of l = "<<l.size()<<endl;
+    cout<<"capacity of m = "<<m.size()<<endl;
+    m.erase(m.begin());
+    for(int i:m)
+    cout<<i<<" ";
+    cout<<"capacity of m = "<<m.size()<<endl;
+}
