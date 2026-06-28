062501683621ac92901eca6784688aea6ca9fa9a
Sadique Anwar <163935036+sadique9334@users.noreply.github.com>
2026-06-28 09:12:47 +0530

    Implement basic map operations in C++
    This program demonstrates the use of a map in C++, including insertion, iteration, and erasure of elements.

--- STL tools in CPP/map.cpp
+++ STL tools in CPP/map.cpp
@@ -1,1 +1,23 @@
-#
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    map<int,string>m;
+    m[1]="sadique";
+    m[3]="anwar";
+    m[8]="aimann";
+    m[5]="ayesha";
+    for(auto i:m){
+        cout<<i.first<<endl;
+    }
+     cout<<endl;
+    m.insert({9,"laptop"});
+    for(auto i:m){
+        cout<<i.first<<" - "<<i.second<<endl;
+    }
+    cout<<endl;
+    m.erase(5);
+ for(auto i:m){
+        cout<<i.first<<" - "<<i.second<<endl;
+    }
+}
