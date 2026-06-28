12d4a69d5f711a49b627f3a7d4468378e58f36e2
Sadique Anwar <163935036+sadique9334@users.noreply.github.com>
2026-06-28 08:10:38 +0530

    Implement basic queue operations in C++
    push and pop operations

--- STL tools in CPP/queue.cpp
+++ STL tools in CPP/queue.cpp
@@ -1,1 +1,19 @@
-#
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<string>q;
+    q.push("sadique");
+    q.push("anwar");
+    q.push("ayesha");
+    q.push("aimann");
+
+    cout<<"1st element = "<<q.front()<<endl;
+    cout<<"last element = "<<q.back()<<endl;
+
+    q.pop();
+    cout<<"1st element = "<<q.front()<<endl;
+    cout<<"last element = "<<q.back()<<endl;
+
+
+}
